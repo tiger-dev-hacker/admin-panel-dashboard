83a4164edcfce499d797cbcee2bdf08668589415
Suril Waghmare <waghmaresuril@gmail.com>
2025-08-02 19:25:53 -0400

    Created dashboard components

--- admin-dashboard/src/pages/home/Home.jsx
+++ admin-dashboard/src/pages/home/Home.jsx
@@ -1,18 +1,29 @@
+import Featured from '../../components/featured/Featured';
 import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Widget from '../../components/widgets/Widget';
 import './home.scss';
-
+import Chart from '../../components/chart/Chart';
 const Home = () => {
   return (
-      <div className="home">
+    <div className="home">
       <Sidebar />
-      <div className='homeContainer'>
+      <div className="homeContainer">
         <Navbar />
-        
-        home container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earnings" />
+          <Widget type="balance" />
+        </div>
+
+        <div className="charts">
+          <Featured />
+          <Chart/>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Home;
